8b00345c4eb8b3b5eb0b04b36e826d6f9548703f
yerassyl <tleugazy98@gmail.com>
2021-01-20 22:59:18 +0600

    sdsdsds

---
+++ runTest.bat
@@ -0,0 +1,3 @@
+@echo off
+cmd /k "cd /d C:\Users\anely\PycharmProjects\parserProjectPython\myvenv\Scripts & activate & cd /d C:\Users\anely\PycharmProjects\parserProjectPython & python test.py"
+pause
