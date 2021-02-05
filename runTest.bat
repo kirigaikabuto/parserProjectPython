568e953eabc8d9761c3bb060968e32c95bbccc73
kirigaikabuto <tleugazy98@gmail.com>
2021-02-05 15:56:32 +0600

    feat:Add lal

--- runTest.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cmd /k "cd /d C:\Users\anely\PycharmProjects\parserProjectPython\myvenv\Scripts & activate & cd /d C:\Users\anely\PycharmProjects\parserProjectPython & python test.py"
-pause
